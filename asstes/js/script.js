15891b1e923c69f2369c82004fc5e84b49703e16
Ujjol Roy <uroy11272@gmail.com>
2023-12-02 11:38:29 +0600

    update

---
+++ asstes/js/script.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        items:3,
+        loop: true,
+        center: true,
+        nav: true,
+        dots: false,
+        navText: ['<i class="fa-solid fa-arrow-left"></i>','<i class="fa-solid fa-arrow-right"></i>']
+    });
+});
